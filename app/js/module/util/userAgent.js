254340de1c1c903d8dff28c2995585b3d57dd636
wuguzi <puxiao.wh@alibaba-inc.com>
2016-05-02 23:14:56 +0800

    更新captch、switch模块

---
+++ app/js/module/util/userAgent.js
@@ -0,0 +1,25 @@
+/*
+ 查询是否是移动端
+ userAgent.isMobile() //boo
+ */
+const userAgent = function() {}
+
+userAgent.isMobile = function() {
+    mobileArr: ["Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod"];
+    return this.mobileArr.some(function(v) {
+        return window.navigator.userAgent.indexOf(v) > 0 ? true : false;
+    });
+}
+
+userAgent.identify = function() {
+    const userAgent = window.navigator.userAgent;
+    if(userAgent.includes('Android')) {
+        return 'android';
+    } else if(userAgent.includes('iPhone')) {
+        return 'ios';
+    } else {
+        return 'windows';
+    }
+}
+
+module.exports = userAgent;
